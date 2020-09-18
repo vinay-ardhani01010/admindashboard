cddb62f911e750eeab2f6c8c6f7ae5101dd58813
vinay <58105215+vinay-ardhani01010@users.noreply.github.com>
2020-09-18 16:06:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,45 @@ router.route('/admindashboard').get(isUser,(req, res) => {
     }); 
   });
 });
+//ADMIN DASHBOARD AUTH PRODUCTS
+
+router.route('/authproducts').get(isUser,(req, res) => { 
+  Product.find({isAuthorised: true}, function(err, data){
+    Product.find().distinct('category', function(error, category) {
+      Product.find().distinct('brand', function(error, brand) {
+        Product.find().distinct('vendorlocation', function(error, location) {
+         
+          res.render('authorized', {
+            products : data,
+            brand : brand,
+            cats : category,
+            location : location,
+            feature : data.slice(0,3),
+         });
+        });
+      });
+    }); 
+  });
+});
+//UNAUTHARIZED PRODUCTS
+router.route('/unauthproducts').get(isUser,(req, res) => { 
+  Product.find({isAuthorised: false}, function(err, data){
+    Product.find().distinct('category', function(error, category) {
+      Product.find().distinct('brand', function(error, brand) {
+        Product.find().distinct('vendorlocation', function(error, location) {
+         
+          res.render('unauthorize', {
+            products : data,
+            brand : brand,
+            cats : category,
+            location : location,
+            feature : data.slice(0,3),
+         });
+        });
+      });
+    }); 
+  });
+});
 
 // Vendor Dashboard
 router.get('/vendordashboard',isVendor, (req, res) => res.render('Vendordashboard'));
@@ -117,51 +156,55 @@ router.get('/filter',(req, res)=>{
     })
     })
   }
-  /*
-  if (req.query.location){
-    console.log('ti')
-      const userlocation = req.query.location;
-      Product.find( li,function(err,data){
-          console.log(data[0])
-          res.render('dashboard',{products : data.slice(0,6) })
-          })
+})
+
+
+  //FILTER ADMIN DASHBOARD
+
+  router.get('/filteradmin',(req, res)=>{
+    console.log(req.body)
+    var querylist = req.query;
+    if(req.query.price){
+      var li = req.query;
+      var n=-1;
+      if (req.query.price=='low'){
+        n = 1
       }
-  if (req.query.price){
-      console.log("yep")
-          if (req.query.price == "high"){
-                  Product.find().sort({price:-1})
-                  .then((err,products) => {res.render('dashboard', {products :data[0]})
+      delete li.price 
+    Product.find( li).sort({price:n}).exec( (err,data)=>{
+      Product.find().distinct('category', function(error, category) {
+        Product.find().distinct('brand', function(error, brand) {
+          Product.find().distinct('vendorlocation', function(err, location) {            
+                res.render('admindashboard', { 
+                  products : data.slice(0,3),
+                  brand : brand,
+                  cats : category,
+                  location : location
+          });
+        });
+      });
+      })
+      })
+    }
+    else {
+      Product.find({isAuthorised:true},(err,data)=>{
+        Product.find(querylist,function (err,data){
+          Product.find().distinct('category', function(error, category) {
+            Product.find().distinct('brand', function(error, brand) {
+              Product.find().distinct('vendorlocation', function(err, location) {
+                    res.render('admindashboard', { 
+                      products : data,
+                      brand : brand,
+                      cats : category,
+                      location : location
+              });
+            });
+          });
           })
-      }
-          else {
-      
-      Product.find().sort({price:1})
-      .then((err,products) => {res.render('dashboard', {products : data[0]})
-          })
-      }
-  }
-  
-  if(req.query.category)
-  {
-  const category = req.query.category
-  Product.find( {category: category},(err, data)=>{
-     setTimeout(() => {
-      console.log("tharun")
-      console.log(data[0])
-      res.render('dashboard',{products : data.slice(0,6) })
-     }, 5000);
+        })
+        }
+      )
+    }
   })
-
-}
-if (req.query.brand)
-  {
-const brandname = req.query.brand
-Product.find( {brand: brandname },(err, data)=>{
-  res.render('dashboard',{products : data[0] })
-})
-  }
-  */
-
-})
-
+  
 module.exports = router;
